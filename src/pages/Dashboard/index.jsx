b408193dce978cdebd190a8cfda4f6b77fb7333d
jrtxr <juniorteixeira1805@gmail.com>
2023-07-30 10:40:45 -0300

    Criar página de dashboard

---
+++ src/pages/Dashboard/index.jsx
@@ -0,0 +1,18 @@
+import { TodoZone } from "../../components/TodoZone";
+import "./styles.css";
+
+const DEFALUT_TODOLIST_ZONES = [
+  { title: "A fazeres", status: "todos" },
+  { title: "Fazendo", status: "doing" },
+  { title: "Concluídas", status: "complete" },
+];
+
+export const Dashboard = () => {
+  return (
+    <main>
+      {DEFALUT_TODOLIST_ZONES.map(({ status, title }, index) => (
+        <TodoZone key={`zone-${index}`} status={status} title={title} />
+      ))}
+    </main>
+  );
+};
